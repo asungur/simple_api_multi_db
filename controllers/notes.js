996d8aff0d6028b1d2f52d9bf754cca15d8d52e9
Alican Sungur <sunguralican@gmail.com>
2022-01-25 18:06:51 +0000

    setup mongoose portion of the app

---
+++ controllers/notes.js
@@ -0,0 +1,64 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+// ROUTES USING MONGO
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2020-01-10T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2020-01-10T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2020-01-10T19:20:14.298Z",
+    important: true
+  }
+]
+
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes.map(note => note.toJSON()))
+})
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important === undefined ? false : body.important,
+    date: new Date()
+  })
+  const savedNote = await note.save()
+
+  response.json(savedNote)
+})
+
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+module.exports = notesRouter
---
+++ controllers/todos.js
@@ -0,0 +1,66 @@
+const todosRouter = require('express').Router()
+
+// Return all todos
+todosRouter.get('/', async (request, response) => {
+  let todos = await response.locals.store.sortedTodos()
+  response.json(todos)
+})
+// Return a single todo
+todosRouter.get("/:todoId", async (request, response) => {
+  const todoId = request.params.todoId
+  let todo = await response.locals.store.loadTodo(todoId)
+  if (todo) {
+    response.json(todo)
+  } else {
+    response.status(404).end()
+  }
+})
+// Toggle completion status of a todo
+todosRouter.post("/:todoId/toggle", async (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  let todoId = body.todo
+  let toggled = await response.locals.store.toggleDoneTodo(+todoId);
+  if (!toggled) {
+    return response.status(400).json({
+      error: 'not found'
+    })
+  }
+  let todo = await response.locals.store.loadTodo(+todoId)
+
+  response.json(todo)
+})
+// Delete a todo
+todosRouter.post("/:todoId/destroy", async (request, response) => {
+  let todoId = request.params
+  let deleted = await response.locals.store.deleteTodo(+todoId);
+  if (!deleted) {
+    return response.status(400).json({
+      error: 'not found'
+    })
+  }
+  response.status(204).end()
+});
+// Create a new todo and add it to the specified list
+todosRouter.post("/", async (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  let created = await response.locals.store.createTodo(todoTitle)
+  if (!created) {
+    return response.status(400).json({
+      error: 'Todo not created for some reason'
+    })
+  }
+  response.redirect('/')
+})
+
+module.exports = todosRouter
